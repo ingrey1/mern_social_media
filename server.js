70865612571e71a922d488a9ec619a51cfb1e0b7
Jason Mortensen <ingrey1@gmail.com>
2019-05-09 23:18:02 -0700

    added api users, posts, profile, auth routes

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
+const app = express();
 
-const app = express()
-
+// connect to db
+connectDB();
 
 app.get('/', (req, res) => {
-	res.send("server running")
-})
+  res.send('server running');
+});
 
-const PORT = process.env.PORT || 5000
+
+// define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/auth', require('./routes/api/auth'))
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-	console.log(`Server started on ${PORT}`);
+  console.log(`Server started on ${PORT}`);
 });
