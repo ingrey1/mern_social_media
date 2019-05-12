8a1cbc3ee9eda47fa93474428e5d37cdd21dcdbb
Jason Mortensen <ingrey1@gmail.com>
2019-05-12 08:48:46 -0700

    Added Profile mongoose model

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,34 +1,25 @@
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-
-
 module.exports = function(req, res, next) {
+  // get the token from header
 
-	// get the token from header
+  const token = req.header('x-auth-token');
 
-	const token = req.header('x-auth-token');
+  // if no token
 
+  if (!token)
+    return res.status(401).json({ msg: 'no token, authorization denied' });
 
-	// if no token
+  // verify token
 
-	if (!token) return res.status(401).json({msg: 'no token, authorization denied'})
+  try {
+    const decodedToken = jwt.verify(token, config.get('jwtSecret'));
 
+    req.user = decodedToken.user;
 
-	// verify token
-	
-	try {
-
-		const decodedToken = jwt.verify(token, config.get("jwtSecret"));
-
-		req.user = decodedToken.user;
-		
-		next();
-
-
-	} catch(err) {
-
-		res.status(401).json({msg: 'token not valid'})
-
-	}	
-}
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'token not valid' });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,37 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../middleware/auth')
+const auth = require('../../middleware/auth');
 const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
 
-
-
-const User = require('../../models/User')
-
+const User = require('../../models/User');
 
 // @route  GET api/auth
 // @desc   jwt authentication
 // @access public
 router.get('/', auth, async (req, res) => {
-  
-	// grab user from db
+  // grab user from db
 
-	try {
-		console.log(req.user)
-		const user = await User.findById(req.user.id).select('-password')
-		res.json(user)
-
-	} catch(err) {
-
-		console.error(err.message)
-		res.status(500).send('Server Error')
-
-	}
-
-
-
+  try {
+    console.log(req.user);
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route  Post api/auth
@@ -40,12 +30,8 @@ router.get('/', auth, async (req, res) => {
 router.post(
   '/',
   [
-   
     check('email', 'Please include a valid email').isEmail(),
-    check(
-      'password',
-      'Please is required'
-    ).exists()
+    check('password', 'Please is required').exists()
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -53,7 +39,7 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const {email, password } = req.body;
+    const { email, password } = req.body;
     try {
       // see if user exists by email
       let user = await User.findOne({ email: email });
@@ -63,23 +49,23 @@ router.post(
           .json({ errors: [{ msg: 'invalid credentials' }] });
       }
 
-
       // see if password for email is valid
-      const validPassword = await bcrypt.compare(password, user.password)
+      const validPassword = await bcrypt.compare(password, user.password);
 
       if (!validPassword) {
-      	return res.status(400).json({ errors: [{ msg: 'invalid credentials' }] })
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'invalid credentials' }] });
       }
-
 
       // create new token for user who logs in
 
       const payload = {
-      	user: {
-      		id: user.id
-      	}
-      }    	
-    
+        user: {
+          id: user.id
+        }
+      };
+
       jwt.sign(
         payload,
         config.get('jwtSecret'),
@@ -90,13 +76,12 @@ router.post(
           if (err) {
             throw err;
           }
-          user.password = undefined
-          res.json({ 
+          user.password = undefined;
+          res.json({
+            user: user,
 
-          	user: user,
-
-
-          	token: token });
+            token: token
+          });
         }
       );
     } catch (err) {
@@ -105,7 +90,5 @@ router.post(
     }
   }
 );
-
-
 
 module.exports = router;
