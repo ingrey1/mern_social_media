93088b9284ff269504c4ed41b9c90c90092c79e8
Jason Mortensen <ingrey1@gmail.com>
2019-05-17 09:17:08 -0700

    Added routes to /api/posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -28,15 +28,19 @@ router.post(
       if (!errors.isEmpty())
         return res.status(400).json({ msg: 'invalid credentials' });
 
-      const { title, text, name, avatar } = req.body;
+      const { title, text } = req.body;
 
       const postFields = {};
+
+      const user = await User.findOne({
+        _id: req.user.id
+      });
 
       postFields.user = req.user.id;
       postFields.title = title;
       postFields.text = text;
-      postFields.name = name;
-      postFields.avatar = avatar;
+      postFields.name = user.name;
+      postFields.avatar = user.avatar;
 
       const newPost = new Post(postFields);
       await newPost.save();
@@ -48,5 +52,54 @@ router.post(
     }
   }
 );
+
+// @route  DELETE api/posts/:post_id
+// @desc   delete post by id
+// @access private
+
+router.delete('/:post_id', auth, async (req, res) => {
+  try {
+    const post = await Post.deleteOne({ _id: req.params.post_id });
+    if (post.deletedCount !== 1)
+      return res.status(500).json({ msg: 'invalid credentials' });
+
+    return res.json({ msg: 'post deleted' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+});
+
+// @route  GET api/posts/:post_id
+// @desc   get post by id
+// @access private
+
+router.get('/:post_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findOne({ _id: req.params.post_id });
+    console.log(post);
+    if (!post) return res.status(400).json({ msg: 'invalid credentials' });
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    console.error(err.message);
+    return res.status(500).send('server error');
+  }
+});
+
+// @route  GET api/posts
+// @desc   get all the posts
+// @access public
+
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find({});
+
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+});
 
 module.exports = router;
