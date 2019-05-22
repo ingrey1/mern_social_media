8698496c5c3f41dc663fe5110d9c5fe33c51c6d9
Jason Mortensen <ingrey1@gmail.com>
2019-05-22 07:28:21 -0700

    bootstrapped client with createreactapp

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+const App = props => {
+  return (
+    <Fragment>
+      <h1>App</h1>
+    </Fragment>
+  );
+};
+
+export default App;
