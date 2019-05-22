a0412e9d48e1c9e970c978cc5ee55f5d45631748
Jason Mortensen <ingrey1@gmail.com>
2019-05-22 10:34:17 -0700

    added alert reducer and alert actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,10 @@
 import { combineReducers } from 'redux';
+import alert from './alert'
+const rootReducer = combineReducers({
 
-const rootReducer = combineReducers({});
+	alert
+
+
+});
 
 export default rootReducer;
